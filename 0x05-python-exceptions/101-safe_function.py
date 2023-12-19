ef5e6fc600a34061741fcfeafde1737ecb3f0506
Young-proflat <olatunbosunlateef6@gmail.com>
2023-12-18 19:58:22 -0800

    Error

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,11 +2,11 @@
 import sys
 
 
-def safe_print_integer_err(value):
-    from sys import stderr
+def safe_function(fct, *args):
     try:
-        print("{:d}".format(value))
-        return True
-    except (ValueError, TypeError):
-        print("Exception: {}".format(sys.exc_info()[1]), file=stderr)
-        return False
+        return fct(*args)
+    except Exception as ex:
+        sys.stderr.write("Exception: ")
+        sys.stderr.write(ex.args[0])
+        sys.stderr.write("\n")
+        return None
