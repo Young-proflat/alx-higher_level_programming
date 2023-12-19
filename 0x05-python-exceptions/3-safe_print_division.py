84498b0fc3a629e54ef970364dd2497c247ae128
Young-proflat <olatunbosunlateef6@gmail.com>
2023-12-18 19:08:30 -0800

    Error

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a,b):
-    result = 0
     try:
-        t
-        result = a/b
+        result = a / b
     except (ZeroDivisionError, TypeError):
         result = None
     finally:
         Print('Inside result: {}'.format(result))
         return result
+
