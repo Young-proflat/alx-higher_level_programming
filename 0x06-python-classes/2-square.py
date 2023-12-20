abdf604dea4a1ea3a0b4c3a91929f7a8e52e3059
Young-proflat <olatunbosunlateef6@gmail.com>
2023-12-20 10:36:39 -0800

    Classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,7 +4,6 @@ class Square:
     """ Initializing data """
     def __init__(self, size=0):
         """" sets the size to a value """
-        self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
