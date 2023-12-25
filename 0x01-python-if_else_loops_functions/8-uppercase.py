b10b5c4e049666f6bda8cb4c11c4a45a4c7a794b
Young-proflat <olatunbosunlateef6@gmail.com>
2023-12-24 19:13:01 -0800

    condition

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-""" accept string and convert in to a capital letter """
 def uppercase(str):
-    """ accept an argument and saves it in result """
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
             i = chr(ord(i) - 32)
