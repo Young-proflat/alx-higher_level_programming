855733487ea3c3b9272a73cfd99ebf8fd2c14b48
Young-proflat <olatunbosunlateef6@gmail.com>
2023-12-24 18:51:05 -0800

    condition_loop

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,10 +2,9 @@
 """ accept string and convert in to a capital letter """
 def uppercase(str):
     """ accept an argument and saves it in result """
-    result = " "
-    """ iterate through the argument """
     for i in str:
         if ord(i) >= 65:
-            result += chr(ord(i) - 32)
-    return result
+            result = chr(ord(i) - 32)
+         print('{}'.format(result), end= '')
+    print()
 
