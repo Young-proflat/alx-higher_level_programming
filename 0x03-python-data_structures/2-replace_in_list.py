b212d75440ca35a53a4aaaff96b3c5a76af75bcc
Young-proflat <olatunbosunlateef6@gmail.com>
2024-01-20 13:49:19 -0800

    replace an element

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that replace an element from a list"""
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list) - 1:
+        return (my_list)
+    my_list[idx] = element
+    return (my_list)
