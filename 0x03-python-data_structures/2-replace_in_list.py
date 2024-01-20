25765b38207db59935d0cf3795745248a97493aa
Young-proflat <olatunbosunlateef6@gmail.com>
2024-01-20 13:53:29 -0800

    replace an intger

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """function that replace an element from a list"""
-
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list) - 1:
         return (my_list)
