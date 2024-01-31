756fd2df0cbdfab6fe18a577036145e311dd9a0c
Young-proflat <olatunbosunlateef6@gmail.com>
2024-01-31 14:47:00 -0800

    classes

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" Creating a square class """
+
+
+class Square:
+    """
+    Defining a class square
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes the data."""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Returns the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size to a value."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Retrieves the position."""
+        return self.__position
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints to stdout the square with the character #,
+        at the position given by the position attribute.
+        """
+        if self.__size == 0:
+            print()
+            return
+        for y in range(0, self.__position[1]):
+            print()
+        for i in range(0, self.__size):
+            for x in range(0, self.__position[0]):
+                print(" ", end="")
+            for j in range(0, self.__size):
+                print("#", end="")
+            print()
