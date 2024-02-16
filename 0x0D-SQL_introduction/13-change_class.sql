2e8b10969d982cf06b941ec6208c488a30314ecc
Young-proflat <olatunbosunlateef6@gmail.com>
2024-02-15 16:21:57 -0800

    me

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,4 +1,4 @@
 -- script that removes all records from second_table of database hbtn_0c_0
 -- deleting all scores <= 5 from the table
 DELETE FROM `second_table`
-WHERE `score` < = 5;
+WHERE `score` <= 5;
