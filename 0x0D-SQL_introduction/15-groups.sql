1b752d521b032a5e3dcd72521f2997d158108510
Young-proflat <olatunbosunlateef6@gmail.com>
2024-02-15 16:10:52 -0800

    me

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,6 @@
 -- scripts the number of record with the same score in the table
 -- count number of score in apperance 
-SELECT `score`, COUNT (*) AS number
+SELECT `score`, COUNT (*) AS `number`
 FROM `second_table`
 GROUP BY `score`
 ORDER BY `number` DESC;
