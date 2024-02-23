3e4882525e5cad77c26ec5f11b71b8f4428db1f1
Young-proflat <olatunbosunlateef6@gmail.com>
2024-02-22 22:51:08 -0800

    testing

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def say_my_name(first_name, last_name=""):
+    """ function print the first_name and last_name """
+    if first_name != str or fisrt_name = "":
+        raise TypeError('first_name must be a string')
+    if last_name != str or last_name = "":
+        raise TypeError('last_name must be a string')
+return (f' My name is {first_name} {last_name}')
