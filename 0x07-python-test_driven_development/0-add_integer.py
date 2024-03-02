31a935a459cb547f8da0698636bd7a5f8a7d1ebb
Young-proflat <olatunbosunlateef6@gmail.com>
2024-03-02 03:43:10 -0800

    Test_me

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,8 +4,8 @@
 
 def add_integer(a, b=98):
     """A function that return int(a) + int(b)."""
-    if type(a) is not an int and type(a) is not float:
+    if type(a) is not int and type(a) is not float:
         raise TypeError('a must be an integer')
-    if type(b) is not an int and type(b) is not float:
+    if type(b) is not int and type(b) is not float:
         raise TypeError('b must be an integer')
-    return (int(a) + int(b))
+    return int(a) + int(b)
