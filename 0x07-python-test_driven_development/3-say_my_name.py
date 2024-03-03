2321b44dd19c3d373b11f1c7ff93e7722d018812
Young-proflat <olatunbosunlateef6@gmail.com>
2024-03-02 17:06:19 -0800

    Test_me

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+"""
+A function that prints My name is <first name> <last name>
+"""
+
+
 def say_my_name(first_name, last_name=""):
-    """ function print the first_name and last_name """
-    if first_name != str or fisrt_name = "":
-        raise TypeError('first_name must be a string')
-    if last_name != str or last_name = "":
-        raise TypeError('last_name must be a string')
-return (f' My name is {first_name} {last_name}')
+    """function that print complete name"""
+    if type(first_name) != str or first_name == "":
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    """print(f"{My name is {first_name} {last_name}")"""
+    print("My name is {} {}".format(first_name, last_name))
