4c1941edcafea31f01c89d8098c2fb32550ab2fb
Young-proflat <olatunbosunlateef6@gmail.com>
2024-03-02 17:38:24 -0800

    Test

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-""" A function that print a square with a character #"""
+""" A  function that prints a square with the character #."""
 
 
 def print_square(size):
-    """A function that prints the square of a size"""
+    """A function that prints a square with the character #."""
     if type(size) is not int:
-        raise TypeError('size must be an int')
+        raise TypeError("size must be an integer")
     if size < 0:
-        raise ValueError('size must be >= 0')
+        raise ValueError("size must be >= 0")
     for i in range(size):
         for j in range(size):
-            print('#', end="")
+            print("#", end="")
         print()
-
