64f532c9348dd8a5dbcb036578f7e2b2afd1ca68
Young-proflat <olatunbosunlateef6@gmail.com>
2024-03-06 08:06:02 -0800

    python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """function that reads a text file (UTF8) and print out an output"""
-    with open(filename, encoding ="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
