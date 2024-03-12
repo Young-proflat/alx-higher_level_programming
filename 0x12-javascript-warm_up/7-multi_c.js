093e4472c411fb5bd1b0a718ce87c313a5a9987b
Young-proflat <olatunbosunlateef6@gmail.com>
2024-03-11 21:28:17 -0700

    Javascript

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const x = isNaN(process.argv[2]);
-if (x === true) {
+const X = isNaN(process.argv[2]);
+if (X === true) {
   console.log('Missing number of occurences');
 } else {
-  for (let x = 0; x < x; x++) {
+  for (let x = 0; x < X; x++) {
     console.log('C is fun')
   }
 }
