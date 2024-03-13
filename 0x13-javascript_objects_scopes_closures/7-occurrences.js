78752e6ef51df0097a5087e436a19f8e0ab1d867
Young-proflat <olatunbosunlateef6@gmail.com>
2024-03-13 04:55:54 -0700

    Javscript

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  
   for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
       count++;
