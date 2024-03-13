b5af7adff1d3993eea57e7f5ec49464fa1ca3b99
Young-proflat <olatunbosunlateef6@gmail.com>
2024-03-13 05:04:34 -0700

    Javascript

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+exports.esrever = functon(list) {
+  const reversed_list = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversedList.push(list[i]);
+  }
+  return reversedList;
+};
