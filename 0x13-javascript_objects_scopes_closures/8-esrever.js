5dd23b6d869047f03e072303197e39e4e683c428
Young-proflat <olatunbosunlateef6@gmail.com>
2024-03-13 18:25:53 -0700

    Jaascript

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-exports.esrever = functon(list) {
-  const reversed_list = [];
-  for (let i = list.length - 1; i >= 0; i--) {
-    reversedList.push(list[i]);
-  }
-  return reversedList;
+exports.esrever = function (list) {
+  return list.reduceRight(function (array, current) {
+    array.push(current);
+    return array;
+  }, []);
 };
